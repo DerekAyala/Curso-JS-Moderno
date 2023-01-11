f62e7f66f6e8e539141ed4f9fd673e29c07c7f3e
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-10 18:55:58 -0600

    Funcion basica

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,9 +1,12 @@
-alert("Hola mundo");
-console.log("Hola mundo");
 // comentario en una sola linea
 
 /* 
-    COMENTARIO MULTILINEA
-    asdasd
-    asdasdasd
+COMENTARIO MULTILINEA
+asdasd
+asdasdasd
 */
+
+function hola(){
+    alert("Hola mundo");
+    console.log("Hola mundo");
+}
