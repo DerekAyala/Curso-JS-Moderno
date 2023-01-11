92aa76fe341bd156eefa2244d22edbfcdbc69370
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-11 13:36:10 -0600

    Crear Strings

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,9 @@
+// dos formas de crear cadenas de texto
+
+const producto = "Monitor de 20\"";
+const producto2 = String("monitor");
+const producto3 = new String("monitor"); 
+
+console.log(producto)
+console.log(producto2)
+console.log(producto3)
