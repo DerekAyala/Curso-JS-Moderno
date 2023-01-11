25eec37e01ca1498eb2a7a17e54d5137d702007e
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-10 21:04:32 -0600

    Variables con var

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,13 @@
+// es un lenguaje de tipo dinamico no se especifica el dato
+// se pueden inicializar sin valor
+var producto;
+producto = "Monitor de 24 pulgadas";
+console.log(producto);
+producto = 'Monitor de 19';
+console.log(producto);
+
+// inicializar multiples variables
+var categoria = 'computadora',
+    marca = 'Marca famosa',
+    calificacion = 5;
+
