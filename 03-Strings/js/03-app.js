1b41f6361677d72e63e218fb6651acd5cb901d18
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-11 13:45:00 -0600

    concatenar strings y template strings

---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,8 @@
+const producto = "Monitor de 20\"";
+
+const precio = '30 usd'
+
+console.log(producto.concat(precio));
+console.log(producto.concat("en descuento"));
+console.log(producto +" "+ precio);
+console.log(`el producto ${producto} tiene un  ${precio}`);
