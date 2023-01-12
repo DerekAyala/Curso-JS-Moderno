5ab8b9c6b88bc0af7ef69ea365cfc7bd51549ab1
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-12 13:12:37 -0600

    acceder a las propiedades de un objeto

---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,9 @@
+const producto = {
+    nombre: "Monitor 20 de pulgadas",
+    precio: 300,
+    disponible: true
+};
+
+console.log(producto.nombre);
+console.log(producto.precio);
+console.log(producto.disponible);
