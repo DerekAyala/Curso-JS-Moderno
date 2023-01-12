00dd1553e78796237b02c67ddb3301a14feba611
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-11 19:58:56 -0600

    Repeat, Split

---
+++ 03-Strings/js/06-app.js
@@ -0,0 +1,12 @@
+const producto = "Monitor de 20 pulgadas";
+
+const texto = ' en promocion'.repeat(3);
+
+console.log(`${producto} ${texto} !!!!!!`);
+
+const actividad = "Estoy Aprendiendo JavaScript Moderno";
+console.log(actividad.split(" "));
+
+const actividades = "Leer, Caminar, Escuchar Musica, aprender a programar";
+console.log(actividades.split(", "));
+
