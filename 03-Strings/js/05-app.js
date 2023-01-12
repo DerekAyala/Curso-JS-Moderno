5f2cfee3a10f935becd21d26f70e8ceb768fd3cb
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-11 19:51:11 -0600

    Replace, Slice y Substring

---
+++ 03-Strings/js/05-app.js
@@ -0,0 +1,11 @@
+const producto = "Monitor de 20 pulgadas"
+
+console.log(producto.replace(" pulgadas","\""));
+console.log(producto.replace("Monitor","Monitor Curvo"));
+
+console.log(producto.slice(0,10));
+console.log(producto.slice(2,1));
+
+console.log(producto.substring(2,1));
+console.log(producto.substring(0,1));
+console.log(producto.charAt(0));
