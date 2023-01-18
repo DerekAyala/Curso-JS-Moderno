3c733737589c0aab11f3a7988bf87cfdb3026999
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-17 18:51:21 -0600

    Diferencias entre funciones

---
+++ 09-Funciones/js/02-app.js
@@ -0,0 +1,17 @@
+// declaracion de funcion
+
+sumar();
+
+function sumar() {
+    console.log( 2 + 2 );
+}
+
+
+
+// expresion de funcion
+
+const sumar2 = function() {
+    console.log( 3 + 3 );
+}
+
+sumar2();
