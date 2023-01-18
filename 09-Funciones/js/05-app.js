ce0862a003f8d3f6daba12cd2b48b4ed8ce7977b
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-17 19:01:59 -0600

    Argumentos y paremtros

---
+++ 09-Funciones/js/05-app.js
@@ -0,0 +1,11 @@
+function sumar(a, b) { // a y b son parametros
+    console.log( a + b );
+}
+
+sumar(5,2); // a y b son argumentos
+
+function saludar(nombre, apellido) {
+    console.log(`Hola ${nombre} ${apellido}`);
+}
+
+saludar("Derek","Ayala");
