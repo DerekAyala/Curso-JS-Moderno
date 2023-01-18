aa4da8dda8d58b4bd2378e9d6e335be7a64d9131
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-17 18:45:13 -0600

    Crear funciones

---
+++ 09-Funciones/js/01-app.js
@@ -0,0 +1,17 @@
+// declaracion de funcion
+
+
+function sumar() {
+    console.log( 2 + 2 );
+}
+
+sumar();
+
+
+// expresion de funcion
+
+const sumar2 = function() {
+    console.log( 3 + 3 );
+}
+
+sumar2();
