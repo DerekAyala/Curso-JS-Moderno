c29fac949424f21761d08ff567a4bb5f643c71ae
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-17 19:43:48 -0600

    Ventajas de arrow funtions

---
+++ 09-Funciones/js/11-app.js
@@ -0,0 +1,9 @@
+
+
+const aprendiendo = lenguaje => `Aprendiendo ${lenguaje}`;
+
+const aprendiendo2 = (lenguaje, lenguaje2) => `Aprendiendo ${lenguaje} y ${lenguaje2}`;
+
+console.log(aprendiendo('Javascript'));
+
+console.log(aprendiendo2('Javascript','css'));
