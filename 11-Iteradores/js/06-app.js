48c0f9c5053735b51cb00668c19ea3f613f6a0b3
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-19 12:41:05 -0600

    forEach y map

---
+++ 11-Iteradores/js/06-app.js
@@ -0,0 +1,7 @@
+const pendientes = ["tarea",'comer','proyecto','Estudiar'];
+
+pendientes.forEach(pendiente => console.log(pendiente));
+
+const nuevoArreglo = pendientes.map(pendiente => pendiente);
+
+console.log(nuevoArreglo);
