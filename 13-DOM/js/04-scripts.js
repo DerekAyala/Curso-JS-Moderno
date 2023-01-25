9281750a1e1c45a4bd582eaf1b8075ab6684bfd5
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-24 19:57:14 -0600

    query selector

---
+++ 13-DOM/js/04-scripts.js
@@ -0,0 +1,17 @@
+let element;
+
+element = document.querySelector('.card');
+
+console.log(element);
+
+let info;
+
+info = document.querySelector('.premium .info');
+
+console.log(info);
+
+let card;
+
+card = document.querySelector('section.hospedaje .card:nth-child(2)');
+
+console.log(card);
