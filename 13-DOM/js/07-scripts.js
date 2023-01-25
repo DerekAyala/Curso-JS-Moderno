dfcb91bc4ad5176efdbabc349ca3a40928f91b49
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-24 20:31:49 -0600

    cambiar css con javascript

---
+++ 13-DOM/js/07-scripts.js
@@ -0,0 +1,8 @@
+const encabezado = document.querySelector('h1');
+
+encabezado.style.backgroundColor = 'red';
+
+const card = document.querySelector('.card');
+
+card.classList.add('nueva-clase');
+
