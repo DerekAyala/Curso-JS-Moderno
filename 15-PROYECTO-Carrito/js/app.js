1401b1861f1520f8b3bfdda45584682edd2620aa
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-26 18:41:17 -0600

    obtener el curso que se esta seleccionando

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -11,6 +11,20 @@ function cargarEventListeners() {
 function agregarCurso(e) {
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')){
-        console.log(e.target);
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCursoss(cursoSeleccionado);
     }
+}
+
+function leerDatosCursoss(curso) {
+    console.log(curso);
+
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoCurso);
 }
