5d28eea7ead8be594607a1675664d7298579a069
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-31 12:47:06 -0600

    Inicializacion de nuevo proyecto de simulador de correos - seleccionar elementos de la interfaz - agregarles sus respectivos eventos - validar que no esten vacios

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+    // seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+
+    inputEmail.addEventListener('blur', validar);
+    inputAsunto.addEventListener('blur', validar);
+    inputMensaje.addEventListener('blur', validar);
+
+    function validar(e) {
+        if (e.target.value.trim() === '') {
+            console.log('vacio')
+        } else {
+            console.log(e.target.value)
+        }
+    }
+});
