0173d9fd800c659c463a44c1ea1cc59fc591e166
DerekAyala <derek.ayala12345@hotmail.com>
2023-01-31 13:19:27 -0600

    agregar mensaje de error

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     inputEmail.addEventListener('blur', validar);
     inputAsunto.addEventListener('blur', validar);
@@ -10,9 +11,16 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function validar(e) {
         if (e.target.value.trim() === '') {
-            console.log('vacio')
+            mostrarAlerta();
         } else {
             console.log(e.target.value)
         }
+    }
+
+    function mostrarAlerta(){
+        const error = document.createElement('P');
+        error.textContent = 'Hubo un error...';
+        error.classList.add('bg-red-600', 'text-white','p-2','text-center');
+        formulario.appendChild(error);
     }
 });
