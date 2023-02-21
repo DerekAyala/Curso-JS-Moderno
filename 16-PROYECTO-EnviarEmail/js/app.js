ef9b615fc599bd17045a731a6f2cc0d75803e5ed
DerekAyala <derek.ayala12345@hotmail.com>
2023-02-20 22:20:27 -0600

    Agregar el spinner

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -11,14 +11,26 @@ document.addEventListener('DOMContentLoaded', function(){
     const formulario = document.querySelector('#formulario');
     const btnEnviar = document.querySelector('#formulario button[type="submit"]');
     const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
 
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
+
+    formulario.addEventListener('submit', enviarEmail);
+
     btnReset.addEventListener('click', function(e) {
         e.preventDefault();
         formulario.reset();
     });
+
+    function enviarEmail(e) {
+        e.preventDefault();
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+        console.log('enviando...')
+
+    }
 
     function validar(e) {
         if (e.target.value.trim() === '') {
